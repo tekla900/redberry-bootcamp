c637fd198bf6f429bac15da018e2eefd1576ecda
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2023-02-06 00:46:28 +0400

    updating resume on the first page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,7 +7,6 @@ function goToLanding() {
     localStorage.clear()
 }
 
-const allInputs = document.getElementsByTagName('input')
 
 const nameInput = document.getElementById('name')
 const surnameInput = document.getElementById('surname')
@@ -18,8 +17,7 @@ const numberInput = document.getElementById('phone_number')
 
 function validateInput(input, regex) {
     let value =  input.value.match(regex)
-
-        
+    
     if (value) {
         input.classList.add('valid')
         input.classList.remove('invalid')
@@ -32,72 +30,107 @@ function validateInput(input, regex) {
 }
 
 function checkForFileUpload(input) {
-    console.log(input.files.length);
-    return input.files.length > 0;
+    return input.files.length > 0
 }
 
-nameInput.addEventListener('blur', () => validateInput(nameInput, /^[ა-ჰ]{2,}$/))
-surnameInput.addEventListener('blur', () => validateInput(surnameInput, /^[ა-ჰ]{2,}$/))
-aboutMeInput.addEventListener('blur',  () => validateInput(aboutMeInput, /.*/))
-emailInput.addEventListener('blur',  () => validateInput(emailInput, /^.*@redberry\.ge$/))
-numberInput.addEventListener('blur',  () => validateInput(numberInput, /^(\+995\s?)?\d{3}\s?\d{3}\s?\d{3}$/))
-fileInput.addEventListener('change', () => checkForFileUpload(fileInput))
+nameInput.addEventListener('blur', () => validateInput(nameInput, nameInput.dataset.regex))
+nameInput.addEventListener('input', updateName)
+
+surnameInput.addEventListener('blur', () => validateInput(surnameInput, surnameInput.dataset.regex))
+surnameInput.addEventListener('input', updateName)
+
+aboutMeInput.addEventListener('blur',  () => validateInput(aboutMeInput, aboutMeInput.dataset.regex))
+aboutMeInput.addEventListener('input', updateAboutMe)
+
+emailInput.addEventListener('blur',  () => validateInput(emailInput, emailInput.dataset.regex))
+emailInput.addEventListener('input', updateEmail)
+
+numberInput.addEventListener('blur',  () => validateInput(numberInput, numberInput.dataset.regex))
+numberInput.addEventListener('input', updateEmail)
+
+// fileInput.addEventListener('change', () => checkForFileUpload(fileInput))
+fileInput.addEventListener('change', updateResumeImg)
 
 function validateFirstPage() {
-    let isFormValid = true;
-    
-    let inputs = [nameInput, surnameInput, aboutMeInput, emailInput, numberInput, fileInput];
+    let isFormValid = true
+
+    let inputs = [nameInput, surnameInput, aboutMeInput, emailInput, numberInput, fileInput]
   
     inputs.forEach(input => {
-      if (input.type === 'file') {
-        if (!checkForFileUpload(input)) {
-          isFormValid = false;
+        console.log(input, input.dataset.regex);
+        if (input.type === 'file') {
+            isFormValid = isFormValid && checkForFileUpload(input)
+        } else {
+            isFormValid = isFormValid && validateInput(input, input.dataset.regex)
         }
-      } else {
-        if (!validateInput(input, input.dataset.regex)) {
-          isFormValid = false;
-        }
-      }
     });
     
-    console.log({isFormValid});
-    return isFormValid;
+    return isFormValid
 }
   
 // NAVIGATION
-// let currentTab = 0;
+let currentTab = 0;
+const pages = document.getElementsByClassName('tab')
 
-// // showTab(currentTab);
-// let pages = document.getElementsByClassName('tab')
+showTab(currentTab)
 
-// function showTab(n) {
-//     pages[n].style.display = 'block'
+function showTab(n) {
+    pages[n].style.display = 'block'
+    document.getElementById("prevBtn").style.visibility = (n === 0) ? 'hidden' : 'visible'
 
-//     if (n === 0) {
-//         document.getElementById("prevBtn").style.visibility = 'hidden';
-//     } else {
-//         document.getElementById("prevBtn").style.display = 'visible';
-//     }
-
-//     if(n === (pages.length - 1)) {
-//         document.getElementById("nextBtn").innerHTML = 'ᲓᲐᲡᲠᲣᲚᲔᲑᲐ';
-//         document.getElementById("nextBtn").type = 'submit'
-//     }
-// }
-
-// function nextPrev(n) {
-//     if (n == 1 && !validateForm()) return false;
- 
-//     pages[currentTab].style.display = "none";
-//     currentTab = currentTab + n;
-
-//     if (currentTab >= pages.length) {
-//         document.getElementById("survey__form").submit();
-//         return false;
-//     }
-
-//     showTab(currentTab);
-// }
+    if (n === (pages.length - 1)) {
+        document.getElementById("nextBtn").innerHTML = 'ᲓᲐᲡᲠᲣᲚᲔᲑᲐ'
+        document.getElementById("nextBtn").type = 'submit'
+    }
+}
 
 
+function nextPrev(n) {
+    if (n === 1 && !validateFirstPage()) return false
+    
+    pages[currentTab].style.display = "none"
+    currentTab += n
+    
+    if (currentTab >= pages.length) {
+        document.getElementById("survey__form").submit()
+        return false
+    }
+    
+    showTab(currentTab)
+}
 
+// UPDATE RESUME
+const infoColumn = document.getElementById('resume--div').querySelector('.first--col')
+const imageColumn = document.getElementById('resume--div').querySelector('.resume--photo')
+
+function updateName() {
+    infoColumn.querySelector('.resume--fullname').innerHTML = `${nameInput.value + ' ' + surnameInput.value}`
+}
+
+function updateAboutMe() {
+    infoColumn.querySelector('.resume--about').innerHTML = `
+        <h2 class="resume--about--header">ᲩᲔᲛ ᲨᲔᲡᲐᲮᲔᲑ</h2>
+        <p class="resume--about--info">${aboutMeInput.value}</p>`
+}
+
+function updateEmail() {
+    infoColumn.querySelector('.resume--contact').innerHTML = `
+    <div><i class="fa-solid fa-at"></i> <p class="resume--contact--info">${emailInput.value}</p></div>
+    <div><i class="fa-solid fa-phone"></i> <p class="resume--contact--info">${numberInput.value}</p></div>
+    `
+}
+
+
+function updateResumeImg() {
+    const file = this.files[0];
+    const reader = new FileReader();
+  
+    reader.addEventListener("load", function() {
+      const img = new Image();
+      img.src = reader.result;
+
+      imageColumn.innerHTML = `<img src=${reader.result} class='resume--image'>`
+    });
+  
+    reader.readAsDataURL(file);
+}
